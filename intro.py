cef05e74e7db49e583910f01adadf7d1765a239b
Chukwuebuka Obidike <obidikecgjr@gmail.com>
2024-08-29 19:57:16 +0100

    First changes

---
+++ intro.py
@@ -0,0 +1,19 @@
+# this is how to commit single line commenting in python
+
+"""
+This is how to create a multi line quote
+More lines can even go here
+"""
+
+# This is how to print in Python
+
+print ("Hello Python")
+
+"""
+This is the breakdown of the *print* code above
+*The Print keyword
+*() -- This is called parentheses
+*" " -- This is called a string
+"""
+
+print ("Hello World2")
